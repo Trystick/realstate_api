b4f2a46c9914694dd4e3851be06375c1fd132755
Luong Thanh Cong <luongthanhcong.dh51905352@gmail.com>
2024-01-02 09:04:12 +0700

    capnhatloginapinext

--- controller/auth.js
+++ controller/auth.js
@@ -71,7 +71,7 @@ try {
       httpOnly: true,
       secure: true, // đảm bảo cookie chỉ được gửi qua kết nối an toàn
       sameSite: 'none', // cho phép cookie được gửi trong các request cross-site
-      domain: 'fri-admin-pr-31.onrender.com' // đặt tên miền đầy đủ của dịch vụ của bạn ở đây
+      domain: 'realstate-api-glm4.onrender.com' // đặt tên miền đầy đủ của dịch vụ của bạn ở đây
     })
     .status(200)
     .json({ details: { ...otherDetails }, role });
